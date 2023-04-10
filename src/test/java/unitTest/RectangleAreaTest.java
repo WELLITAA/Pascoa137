f1917c68a64a7b0071783f4b6fe235027e55fe5a
Johnny Kamigashima <johnny@jhk.app>
2023-04-10 20:58:18 -0300

    Adicionado Test coverage

--- src/test/java/unitTest/RectangleAreaTest.java
+++ src/test/java/unitTest/RectangleAreaTest.java
@@ -1,4 +1,4 @@
-package unitTestSprint3;
+package unitTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
